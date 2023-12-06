f492bfdedfbb1f638b9c66cf210b9a0c41456da2
BethJW <bethanyjwilliamson16@gmail.com>
2023-12-06 11:22:48 +0000

    Added JS  to dropdown to update selected timezone

--- src/index.js
+++ src/index.js
@@ -13,5 +13,28 @@ function updateTime() {
   franceTime.innerHTML = franceTimezone.format("h:mm A");
   franceDate.innerHTML = franceTimezone.format("dddd, Do MMMM");
 }
+
+function updateTimezone(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#timezones");
+  citiesElement.innerHTML = `  
+  <div id="timezones">
+          <div class="city-container">
+            <div class="city">
+              <div>${cityName}</div>
+            </div>
+            <div class="time-date">
+              <div class="time">${cityTime.format("h:mm A")}</div>
+              <div class="date">${cityTime.format("dddd, Do MMMM")}</div>
+            </div>
+          </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 60000);
+setInterval(updateTimezone, 60000);
+
+let citySelectElement = document.querySelector("#cities");
+citySelectElement.addEventListener("change", updateTimezone);
